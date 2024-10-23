cf6386520e8094bc959daf2ba04f3338305d30c8
3435 <3435@example.com>
2024-10-23 12:18:41 +0900

    ログイン機能の追加（初期説明用）

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,11 @@ Rails.application.routes.draw do
   resources :grades
   resources :rooms
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/', to: 'login#index', as: 'login'
+# get 'login', to: 'login#index', as: 'login'
+# get 'teacher_schedule/:id', 'teacher_schedule#show', as: 'teacher_schedule'
+
+#  <%= link_to 'ログイン画面へ', login_path %>
+#  <%= link_to teacher.name, teacher_schedule_path(teacher.id) %>
 end

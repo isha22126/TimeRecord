b847b0f961a543165c0312beb33b6e1c1e63f929
3403 <3403@example.com>
2024-11-08 11:42:43 +0900

    教員スケジュール機能のroutingを追加

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/', to: 'login#index', as: 'login'
+  get 'teacher_select', to: 'teacher_select#index', as: 'teacher_select'
+  get 'teacher_schedule/:id/:date', to: 'teacher_schedule#show', as: 'teacher_schedule'
+  get 'teacher_search', to: 'teacher_select#show'
+  post 'teacher_search', to: 'teacher_select#search'
+  get 'teacher_select', to: 'teacher_schedule#show'
 # get 'login', to: 'login#index', as: 'login'
-# get 'teacher_schedule/:id', 'teacher_schedule#show', as: 'teacher_schedule'
 
 #  <%= link_to 'ログイン画面へ', login_path %>
 #  <%= link_to teacher.name, teacher_schedule_path(teacher.id) %>

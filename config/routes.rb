4e1746cfbcf4688a461026236b66549f4859446d
3435 <3435@example.com>
2024-11-08 11:44:48 +0900

    管理者機能のroutingを追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,14 +9,17 @@ Rails.application.routes.draw do
   resources :rooms
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  # ログイン
   get '/', to: 'login#index', as: 'login'
+
+  # 管理者機能
+  get 'admin_home', to: 'login#index', as: 'admin_home'
+
+  # 教員スケジュール関連
   get 'teacher_select', to: 'teacher_select#index', as: 'teacher_select'
   get 'teacher_schedule/:id/:date', to: 'teacher_schedule#show', as: 'teacher_schedule'
   get 'teacher_search', to: 'teacher_select#show'
   post 'teacher_search', to: 'teacher_select#search'
   get 'teacher_select', to: 'teacher_schedule#show'
-# get 'login', to: 'login#index', as: 'login'
 
-#  <%= link_to 'ログイン画面へ', login_path %>
-#  <%= link_to teacher.name, teacher_schedule_path(teacher.id) %>
 end

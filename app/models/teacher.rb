a5fcb0e51d212553f28c84962f6ea455f4eb3962
3435 <3435@example.com>
2024-12-10 15:01:28 +0900

    教員の新規登録のバリデーション

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,4 +1,12 @@
 class Teacher < ApplicationRecord
   belongs_to :room
-  has_many :Schedules
-end
+  has_many :schedules
+
+  validates :name, presence: true
+  validates :kana, presence: true
+  validates :mail, presence: true
+  validates :pw, presence: true
+  
+  validates :mail, uniqueness: true
+
+ end
